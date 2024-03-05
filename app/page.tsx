38ef2926551e3ca16ab667d020de1c1e31fdcfa3
EstradaJairo <destrada@jairosolutions.com>
2024-03-05 09:25:24 +0800

    Initial Commit:Setting Up Boiler Plates

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between max-w-[1920px] px-[149px] py-[100px] w-full">
+      <div>LANDING PAGE</div>
+    </main>
+  );
+}
