9578488802a6ee4bd5ffd7b2fbca6c68d80b8411
jsvllnv <jaszmeineabigaille.villanueva@hcdc.edu.ph>
2024-03-21 15:06:43 +0800

    patients list pagination fix

--- app/(routes)/patient-list/page.tsx
+++ app/(routes)/patient-list/page.tsx
@@ -287,70 +287,71 @@ export default function PatientPage() {
         {/* END OF TABLE */}
       </div>
       {/* pagination */}
-      <div className="mt-5 flex justify-between w-full mx-3">
-        <div>
-          <p className="font-medium text-[14px] items-center">Page 1 of 10</p>
-        </div>
-        <div className="flex items-center">
-          <nav>
-            <div className="flex space-x-px text-sm">
-              <div>
-                <a
-                  href="#"
-                  className="flex border border-px items-center justify-center w-[77px] h-[25px] "
-                >
-                  Prev
-                </a>
+      <div className="mt-5 w-full mx-3">
+        <div className="flex justify-between">
+          <p className="font-medium text-[14px] w-[138px] items-center">
+            Page 1 of 10
+          </p>
+          <div>
+            <nav>
+              <div className="flex -space-x-px text-sm">
+                <div>
+                  <a
+                    href="#"
+                    className="flex border border-px items-center justify-center  w-[77px] h-full"
+                  >
+                    Prev
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="#"
+                    className="flex border border-px items-center justify-center  w-[49px] h-full"
+                  >
+                    1
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="#"
+                    className="flex border border-px items-center justify-center  w-[49px] h-full"
+                  >
+                    2
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="#"
+                    aria-current="page"
+                    className="flex border border-px items-center justify-center  w-[49px] h-full"
+                  >
+                    3
+                  </a>
+                </div>
+
+                <div className="">
+                  <a
+                    href="#"
+                    className="flex border border-px items-center justify-center  w-[77px] h-full mr-5"
+                  >
+                    Next
+                  </a>
+                </div>
+                <div className="flex">
+                  <input
+                    className="ipt-pagination border text-center"
+                    type="text"
+                    placeholder="-"
+                  />
+                  <div className="">
+                    <button className="btn-pagination ">Go </button>
+                  </div>
+                </div>
               </div>
-              <div>
-                {/* wala pay active function */}
-                <a
-                  href="#"
-                  className="flex border border-px items-center justify-center w-[49px] h-[25px] "
-                >
-                  1
-                </a>
-              </div>
-              <div>
-                <a
-                  href="#"
-                  className="flex border border-px items-center justify-center w-[49px] h-[25px] "
-                >
-                  2
-                </a>
-              </div>
-              <div>
-                <a
-                  href="#"
-                  aria-current="page"
-                  className="flex border border-px items-center justify-center w-[49px] h-[25px] "
-                >
-                  3
-                </a>
-              </div>
-              <div>
-                <a
-                  href="#"
-                  className="flex border border-px items-center justify-center w-[77px] h-[25px] mr-2"
-                >
-                  Next
-                </a>
-              </div>
-            </div>
-          </nav>
-          <div className="flex items-center justify-end">
-            <input
-              className="ipt-pagination text-center border ring-1 ring-gray-300 border-gray-100 w-[40px] h-[25px]  "
-              type="text"
-              placeholder="-"
-            />
-            <button className="btn-pagination">Go</button>
+            </nav>
           </div>
         </div>
       </div>
-      {isOpen && (
-        <Modal isModalOpen={isModalOpen} isOpen={isOpen} label="sample label" />
-      )}
     </div>
   );
 }
