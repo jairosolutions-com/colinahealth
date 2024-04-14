fe8697b9bd121da23f7ae5797503468da4d8502c
nachocheez <cheskapg@gmail.com>
2024-04-14 23:14:24 +0800

    smaller screen preview size

--- components/modals/labresults.modal.tsx
+++ components/modals/labresults.modal.tsx
@@ -58,25 +58,29 @@ export const LabResultModal = ({
   // Convert the buffer data to base64 string
 
   const [base64String, setBase64String] = useState("");
-  const [fileType, setFileType] = useState("");
+  const [fileType, setFileType] = useState<string>("");
 
   // Define the effect to update base64String and fileType when fileData changes
   useEffect(() => {
     if (labFiles.length > 0) {
       const file = labFiles[fileIndex];
       setCurrentFile(file);
-      setFileName(fileName);
-      setFileData(fileData);
-
-      // Convert the data to base64 and set the file type
-      const newBase64String = Buffer.from(currentFile.data).toString("base64");
-      setBase64String(newBase64String);
-      console.log("FILE STRING", base64String);
-      const newFileType = currentFile.filename.split(".").pop() as string;
-      setFileType(newFileType);
-      console.log("FILE newFileType", newFileType);
+      setFileName(file.filename); // Set the filename using the file object
+      setFileData(file.data); // Set the file data using the file object
+  
+      // Only proceed with the conversion if currentFile.data and currentFile.filename are defined
+      if (currentFile.data !==null && currentFile.filename !==null ) {
+        // Convert the data to base64 and set the file type
+        const newBase64String = Buffer.from(currentFile.data).toString('base64');
+        setBase64String(newBase64String);
+        console.log('FILE STRING', base64String);
+  
+        const newFileType = currentFile.filename.split('.').pop() as string;
+        setFileType(newFileType);
+        console.log('FILE newFileType', newFileType);
+      }
     }
-  }, [fileIndex, labFiles]);
+  }, [fileIndex, labFiles, fileData, fileName, currentFile]);
   // Update the current file when fileIndex changes
   useEffect(() => {
     setCurrentFile(labFiles[fileIndex]);
@@ -213,7 +217,9 @@ export const LabResultModal = ({
     >
       <div
         className={`bg-[#FFFFFF] rounded-md ${
-          isView ? "max-w-[1200px] w-[1200px] h-[1200px]" : "max-w-[1500px] h-[500px] w-[550px]"
+          isView
+            ? " md:w-[1200px] max-h-[800px] max-w-[1000px] md:h-[1200px]"
+            : " h-[500px] w-[550px]"
         }`}
       >
         <div className="bg-[#ffffff] flex flex-col justify-start rounded-md">
@@ -410,17 +416,16 @@ export const LabResultModal = ({
                         {fileType === "pdf" ? (
                           <iframe
                             src={`data:application/pdf;base64,${base64String}`}
-                            width="800px"
-                            height="1000px"
+                            width="600px"
+                            height="550px"
                             className="shadow-md rounded-lg"
                           ></iframe>
                         ) : (
-                          <iframe  width="800px"
-                          height="1000px"
-                          src={`data:image/${fileType};base64,${base64String}`}>
-
-                        
-                            </iframe>
+                          <iframe
+                            width="600px"
+                            height="550px"
+                            src={`data:image/${fileType};base64,${base64String}`}
+                          ></iframe>
                         )}
                       </div>
 
@@ -447,6 +452,35 @@ export const LabResultModal = ({
                   )}
                 </div>
               </>
+            )}
+            {labFiles.length < 1 && (
+              <div className=" grid grid-cols-1 md:grid-cols-10 gap-4">
+                <div className="md:px-10 mt-5 md:col-span-3 ">
+                  <div className="grid grid-cols-1 gap-x-4 gap-y-4">
+                  
+                       No Files Attached
+                  </div>
+
+                  <div className="mt-5 pb-3">
+                    <button
+                      onClick={() => isModalOpen(false)}
+                      type="button"
+                      className="w-48 px-3 py-2 hover:bg-[#D9D9D9] font-medium rounded-[7px] text-[#000] ring-1 ring-gray-200"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+
+                  <div className="mt-5 pb-3">
+                    <button
+                      onClick={() => isModalOpen(false)}
+                      className="w-48 px-3 py-2 bg-[#1B84FF] hover:bg-[#2765AE] rounded-[7px] text-[#ffff] font-medium"
+                    >
+                      OK
+                    </button>
+                  </div>
+                </div>
+              </div>
             )}
           </>
         )}
