e675665375c1ddb68b3a762da7b9f7d8ee913ffd
kenjanjan <kjliloc@hcdc.edu.ph>
2024-04-15 13:59:33 +0800

    #colina-260-dashboard-integration

---
+++ app/api/medication-logs-api/due-medication-api.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosError } from "axios";
+import { onNavigate } from "@/actions/navigation";
+import { getAccessToken, setAccessToken } from "../login-api/accessToken";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+// Function to get the access token from local storage
+
+export async function fetchDueMedication(
+  term: string,
+  currentPage: number,
+  sortBy: string,
+  sortOrder: "ASC" | "DESC",
+  router: any // Pass router instance as a parameter
+): Promise<any> {
+  const requestData = {
+    term: term,
+    page: currentPage,
+    sortBy: sortBy,
+    sortOrder: sortOrder,
+  };
+  try {
+    console.log("searchPatient", requestData);
+    const accessToken = getAccessToken();
+    if (!accessToken) {
+      throw new Error("Access token not found in local storage");
+    }
+
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    const response = await axios.post(
+      `${apiUrl}/medication-logs/due-medications`,
+      requestData,
+      { headers }
+    );
+
+    console.log(response.data);
+    const dueMedications = response.data;
+    console.log(dueMedications, "patient dueMedications after search");
+    return dueMedications;
+  } catch (error) {
+    if ((error as AxiosError).response?.status === 401) {
+      setAccessToken("");
+      onNavigate(router, "/login");
+      return Promise.reject(new Error("Unauthorized access"));
+    }
+    console.error(
+      "Error searching patient dueMedications:",
+      (error as AxiosError).message
+    );
+  }
+}
