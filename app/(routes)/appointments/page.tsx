24fad3296427206e60063e1c23ec088f97fc631b
kenjanjan <kjliloc@hcdc.edu.ph>
2024-04-19 13:00:58 +0800

    #colina-313-[DEVELOPMENT] - Clicking Tab Labels Only Refreshes Page Instead of Routing to Other Labels.

--- app/(routes)/appointments/page.tsx
+++ app/(routes)/appointments/page.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 import { onNavigate } from "@/actions/navigation";
 import { getAccessToken } from "@/app/api/login-api/accessToken";
 import { useRouter } from "next/navigation";
 
 export default function AppointmentsPage() {
   const router = useRouter();
-  if(!getAccessToken()){
+  if (!getAccessToken()) {
     onNavigate(router, "/login");
   }
   return (
-    <div className="">
-      <p>Appointment List Page</p>
+    <div className="h-full w-full flex items-center justify-center">
+      Appointments
     </div>
   );
 }
--- app/(routes)/due-medications/page.tsx
+++ app/(routes)/due-medications/page.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+'use client'
+import React, { useCallback, useEffect } from 'react'
 
 const DueMedications = () => {
+
   return (
     <div className='h-full w-full flex items-center justify-center'>Due Medications</div>
   )
