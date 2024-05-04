04c623a35fc10a479e7f14e76913f0eea5569926
kenjanjan <kjliloc@hcdc.edu.ph>
2024-05-05 00:26:19 +0800

    login bg color to render first before image loads

--- components/login.tsx
+++ components/login.tsx
@@ -107,7 +107,7 @@ export const Login = () => {
     <div className="w-full h-full flex ">
       <section className="w-full h-full">
         <div className="w-full h-full flex ">
-          <div className="flex w-full items-center h-full justify-center md:z-10 -z-[100]">
+          <div className="flex bg-[#007C85] w-full items-center h-full justify-center md:z-10 -z-[100]">
             <Image
               src="/imgs/login-bg.png"
               alt="login-image"
