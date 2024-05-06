2cf3e237d5f4c5f2733dda1bc6e0e6f545f5641a
kenjanjan <kjliloc@hcdc.edu.ph>
2024-05-06 18:08:10 +0800

    #colina-419-component but allergy data sa

--- app/(routes)/patient-overview/[id]/medical-history/allergies/page.tsx
+++ app/(routes)/patient-overview/[id]/medical-history/allergies/page.tsx
@@ -20,6 +20,7 @@ import { AllergiesModalContent } from "@/components/modal-content/allergies-moda
 import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/components/ui/use-toast";
 import Pagination from "@/components/shared/pagination";
+import PdfDownloader from "@/components/pdfDownloader";
 
 const Allergies = () => {
   const router = useRouter();
@@ -198,107 +199,6 @@ const Allergies = () => {
     setIsErrorOpen(true);
     setIsEdit(false);
   };
-  const handleDownloadPDF = async () => {
-    if (patientAllergies.length === 0) {
-      toast({
-        variant: "destructive",
-        title: "Uh oh! Something went wrong.",
-        description: "Allergy list is empty",
-        action: (
-          <ToastAction
-            altText="Try again"
-            onClick={() => {
-              window.location.reload();
-            }}
-          >
-            Try again
-          </ToastAction>
-        ),
-      });
-    } else {
-      let patientName =
-        patientAllergies[0]?.patient_lastName +
-        ", " +
-        patientAllergies[0]?.patient_firstName +
-        " " +
-        patientAllergies[0]?.patient_middleName;
-      let jsonFile: {
-        Uuid: string;
-        Date: string;
-        Type: string;
-        Allergen: string;
-        Severity: string;
-        Reaction: string;
-        Notes: string;
-      }[] = patientAllergies.map((allergy) => ({
-        Uuid: allergy.allergies_uuid,
-        Date: new Date(allergy.allergies_createdAt).toLocaleDateString(),
-        Type: allergy.allergies_type,
-        Allergen: allergy.allergies_allergen,
-        Severity: allergy.allergies_severity,
-        Reaction: allergy.allergies_reaction,
-        Notes: allergy.allergies_notes,
-      }));
-
-      const patientFullName = patientName;
-
-      printJS({
-        printable: jsonFile,
-        properties: [
-          "Allergy UID",
-          "Date",
-          "Type",
-          "Allergen",
-          "Severity",
-          "Reaction",
-          "Notes",
-        ],
-        type: "json",
-        style: `
-          @page { size: Letter landscape; }
-          @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap');
-          
-          body {
-            font-family: 'Manrope', sans-serif;
-            font-weight:lighter;
-          }
-          hr.new1 {
-            margin-top: -40px;
-            border: 3px solid #007C85;
-          }
-          hr.new2 {
-            border: 1px solid #007C85;
-          }
-
-          p {
-            font-size: 23.57px;
-            font-weight: lighter;
-          }
-          .logo-bg {
-            
-            height: 77.79px;
-            width: 226.48px;
-          }
-        `,
-        header: `<div> 
-                    <div class="logo-bg">
-                      <img src="https://i.imgur.com/LrS9IUe.png" height="30px alt="">
-                    </div>
-                  <div>
-                  <hr class="new1">
-                  <hr class="new2">
-                    <p>${patientFullName}</p>
-                  </div>
-                </div>`,
-        headerStyle: "color: #64748B; font-size:23.57px; font-weight:lighter",
-        gridHeaderStyle:
-          "color: #64748B;  border: 1.18px solid #EEEEEE; text-align:left; padding-left:5px; height:56.05px; font-weight:lighter; ",
-        gridStyle:
-          "border: 1.18px solid #EEEEEE; padding-left:5px; height:71.87px ",
-        documentTitle: "",
-      });
-    }
-  };
 
   return (
     <div className="w-full h-full flex flex-col justify-between">
@@ -334,15 +234,19 @@ const Allergies = () => {
               <p className="text-[18px]">Add</p>
             </button>
 
-            <button className="btn-pdfs gap-2" onClick={handleDownloadPDF}>
-              <Image
-                src="/imgs/downloadpdf.svg"
-                alt=""
-                width={22}
-                height={22}
-              />
-              <p className="text-[18px]">Download PDF</p>
-            </button>
+            <PdfDownloader
+              data={patientAllergies}
+              props={[
+                "Uuid",
+                "Date",
+                "Type",
+                "Allergen",
+                "Severity",
+                "Reaction",
+                "Notes",
+              ]}
+              variant={"Allergy Table"}
+            />
           </div>
         </div>
         <div className="w-full m:rounded-lg items-center">
