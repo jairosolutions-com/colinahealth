d11be41cec60e696421ec85ec46211ea9f8f7704
kenjanjan <kjliloc@hcdc.edu.ph>
2024-05-17 13:14:51 +0800

    #success modal in nurses note

--- components/nurse-drawer.tsx
+++ components/nurse-drawer.tsx
@@ -37,14 +37,17 @@ import { createNotesOfPatient } from "@/app/api/notes-api/notes-api";
 import { toast } from "./ui/use-toast";
 import { ToastAction } from "./ui/toast";
 import { selectPatient } from "@/app/api/patients-api/patientSelect.api";
+import { SuccessModal } from "./shared/success";
 
-const NurseDrawer = ({ setIsSuccessOpen }: any) => {
+const NurseDrawer = () => {
   const router = useRouter();
   const [open, setOpen] = React.useState(false);
   const [patientId, setPatientId] = React.useState("");
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
   const [error, setError] = useState("");
+  const [isSuccessOpen, setIsSuccessOpen] = useState(false);
+  const [isError, setIsError] = useState(false);
   const [patientList, setPatientList] = useState([
     {
       uuid: "",
@@ -77,47 +80,45 @@ const NurseDrawer = ({ setIsSuccessOpen }: any) => {
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     console.log("sub");
-    if (patientId) {
-      setIsSubmitted(true);
-      try {
-        const notes = await createNotesOfPatient(patientId, formData, router);
-        if (notes) {
-          setIsSuccess(true);
-          console.log(isSuccess, "issuccess");
-        }
-
-        setFormData({
-          subject: "",
-          notes: "",
-          type: "nn",
-        });
-        setIsSuccessOpen(true);
-        setOpen(false);
-        setPatientId("");
-      } catch (error: any) {
-        if (error.message == "Network Error") {
-          toast({
-            variant: "destructive",
-            title: "Uh oh! Something went wrong.",
-            description: error.message,
-            action: (
-              <ToastAction
-                altText="Try again"
-                onClick={() => {
-                  window.location.reload();
-                }}
-              >
-                Try again
-              </ToastAction>
-            ),
-          });
-        }
-        console.error("Error adding note:", error);
-        setError("Failed to add note");
+    setIsSubmitted(true);
+    try {
+      const notes = await createNotesOfPatient(patientId, formData, router);
+      if (notes) {
+        setIsSuccess(true);
+        console.log(isSuccess, "issuccess");
       }
-    } else {
-      setError("no patient id");
+
+      setFormData({
+        subject: "",
+        notes: "",
+        type: "nn",
+      });
+
+      onSuccess();
+      setPatientId("");
+    } catch (error: any) {
+      console.log(error, "errrorr1");
+      if (error.message == "Network Error") {
+        toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: error.message,
+          action: (
+            <ToastAction
+              altText="Try again"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              Try again
+            </ToastAction>
+          ),
+        });
+      }
+      console.error("Error adding note:", error);
+      setError("Failed to add note");
     }
+
     setIsSubmitted(false);
   };
 
@@ -156,6 +157,11 @@ const NurseDrawer = ({ setIsSuccessOpen }: any) => {
       setIsSuccess(false);
     }
   }, [isSuccess]);
+
+  const onSuccess = () => {
+    setIsSuccessOpen(true);
+    setOpen(false);
+  };
 
   return (
     <>
@@ -322,31 +328,41 @@ const NurseDrawer = ({ setIsSuccessOpen }: any) => {
               </div>
             </div>
 
-            <DrawerFooter className="flex flex-row justify-end gap-1 mb-2">
+            <DrawerFooter className="flex flex-row justify-end gap-3 mb-2">
               <DrawerClose>
-                <Button
+                <button
                   type="button"
                   disabled={isSubmitted}
-                  variant="outline"
                   className={` cancel-button
-                  ${isSubmitted && " cursor-not-allowed"}
-                  w-[150px] h-[45px]  bg-[#F3F3F3] hover:bg-[#D9D9D9] font-medium text-black  mr-4 rounded-sm`}
+                  ${isSubmitted ? " cursor-not-allowed" : "cursor-pointer"}
+                  w-[150px] h-[45px]  bg-[#F3F3F3] hover:bg-[#D9D9D9] font-medium text-black  rounded-sm`}
                 >
                   Cancel
-                </Button>
+                </button>
               </DrawerClose>
-              <Button
+              <button
                 disabled={isSubmitted}
                 type="submit"
                 className={`
-                ${isSubmitted && " cursor-not-allowed"}
+                ${isSubmitted ? " cursor-not-allowed" : "cursor-pointer"}
                        w-[150px] h-[45px] px-3 py-2 bg-[#007C85] hover:bg-[#03595B]  text-[#ffff] font-medium  rounded-sm`}
               >
                 Submit
-              </Button>
+              </button>
             </DrawerFooter>
           </form>
         </DrawerContent>
+        {isSuccessOpen && (
+          <DrawerClose>
+            <SuccessModal
+              label="Success"
+              isAlertOpen={isSuccessOpen}
+              toggleModal={setIsSuccessOpen}
+              isUpdated={""}
+              setIsUpdated={""}
+            />
+          </DrawerClose>
+        )}
       </Drawer>
     </>
   );
