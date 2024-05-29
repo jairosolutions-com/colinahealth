4f483b03d3d17ccf3a50cafcef14ce55bc041231
kenjanjan <kjliloc@hcdc.edu.ph>
2024-05-29 08:36:46 +0800

    layout color

--- components/dashboard/dbPatientSummary.tsx
+++ components/dashboard/dbPatientSummary.tsx
@@ -17,21 +17,21 @@ const DBPatientSummary = () => {
                 Patient Details
               </h1>
             </div>
-            <div className="h-2/4 bg-[#D9D9D91A] p-5">
-              <h1 className="font-bold text-xl">Medication</h1>
+            <div className="h-2/4 bg-[#D9D9D91A] ">
+              <h1 className="font-bold text-xl bg-violet-500 w-full p-5">Medication</h1>
             </div>
           </div>
           <div className="w-1/2 flex flex-col gap-3">
-            <div className="h-2/4 bg-[#D9D9D91A] p-5">
-              <h1 className="font-bold text-xl">Allergies</h1>
+            <div className="h-2/4 bg-[#D9D9D91A]">
+              <h1 className="font-bold text-xl bg-cyan-500 w-full p-5">Allergies</h1>
             </div>
-            <div className="h-2/4 bg-[#D9D9D91A] p-5">
-              <h1 className="font-bold text-xl">Vital Signs</h1>
+            <div className="h-2/4 bg-[#D9D9D91A] ">
+              <h1 className="font-bold text-xl bg-yellow-500 w-full p-5">Vital Signs</h1>
             </div>
           </div>
         </div>
         <div className="h-2/6  w-full flex flex-col gap-3">
-          <h1 className="font-bold text-xl">Due Medication Graph</h1>
+          <h1 className="font-bold text-xl bg-orange-500 w-full p-5">Due Medication Graph</h1>
           <div className="w-full flex gap-3 h-full">
             <div className="w-1/2 bg-blue-500"></div>
             <div className="w-1/2 bg-green-500"></div>
