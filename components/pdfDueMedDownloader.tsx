fe881a1d04856f091b9cf21ea1382b67b25e1b20
kenjanjan <kjliloc@hcdc.edu.ph>
2024-06-07 11:15:21 +0800

    pdf printjs in serverside to clientside fix

--- components/pdfDueMedDownloader.tsx
+++ components/pdfDueMedDownloader.tsx
@@ -3,11 +3,9 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { toast } from "./ui/use-toast";
 import { ToastAction } from "./ui/toast";
-import printJS from "print-js";
 import DownloadPDF from "./shared/buttons/downloadpdf";
 import { fetchDueMedication } from "@/app/api/medication-logs-api/due-medication-api";
 import { useRouter } from "next/navigation";
-import { print } from "@/lib/utils";
 
 const PdfDueMedDownloader = ({ props, variant}: any) => {
     const router = useRouter();
@@ -71,8 +69,71 @@ const PdfDueMedDownloader = ({ props, variant}: any) => {
         Time: d.medicationlogs_medicationLogsTime,
       }));
 
-      print(jsonFile, props, variant);
+      if (typeof window !== "undefined") {
+        const printJS = (await import("print-js")).default;
+        printJS({
+          printable: jsonFile,
+          properties: props,
+          type: "json",
+          style: `
+            @page { size: Letter landscape; }
+            @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap');
+            
+            body {
+              font-family: 'Manrope', sans-serif;
+              font-weight:lighter;
+            }
+            hr.new1 {
+              margin-top: -40px;
+              border: 3px solid #007C85;
+            }
+            hr.new2 {
+              border: 1px solid #007C85;
+            }
+  
+            p {
+              font-size: 23.57px;
+              font-weight: lighter;
+            }
+            .logo-bg {
+              display:flex;
+              height: 77.79px;
+              width: 226.48px;
+            }
+            .variant {
+              text-align: center;
+              align-items: center;
+              width: 100%;
+              position:absolute;
+            }
+            .w-full {
+              width: 100%;
+            }
+            div {
+              width: 100%;
+            }
+          `,
+          header: `<div class="w-full"> 
+                      <div class="logo-bg w-full">
+                        <img src="https://i.imgur.com/LrS9IUe.png" height="30px alt="">
+                        <div class="variant w-full">${variant}</div>
+                      </div>
+                    <div>
+                    <hr class="new1">
+                    <hr class="new2">
+                    </div>
+                  </div>`,
+          headerStyle: "color: #64748B; font-size:23.57px; font-weight:lighter",
+          gridHeaderStyle:
+            "color: #64748B;  border: 1.18px solid #EEEEEE; text-align:left; padding-left:5px; height:56.05px; font-weight:lighter; ",
+          gridStyle:
+            "border: 1.18px solid #EEEEEE; padding-left:5px; height:71.87px ",
+          documentTitle: "",
+        });
+      }
     }
+  
+    
   };
   return (
     <div onClick={handleDownloadPDF}>
